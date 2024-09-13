987d3377792b3dea98db58ba2dfb12cc28edc870
Tronaluz <lucas_tron@hotmail.com>
2024-09-12 22:55:51 -0300

    fix js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ AOS.init();
 const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
-const contaAsHoras = setInternal(function () {
+const contaAsHoras = setInterval(function() {
     const agora = new Date();
     const timeStampAtual = agora.getTime();
 
@@ -16,10 +16,9 @@ const contaAsHoras = setInternal(function () {
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
     const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMS);
     const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMS) / minutoEmMs);
-    const segundoAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
 
-
-    document.getElementById('contador').innerHTML = '${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s';
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
 
     if (distanciaAteOEvento < 0){
         clearInterval(contaAsHoras);
